7663aed1a0f408f8916384879a586c80ed2b9ef2
Paulo Santos <santospaulor21@gmail.com>
2024-09-02 16:10:38 -0300

    fix: controller

--- 6-java-web/projetos/backend-cidades/src/main/java/br/edu/utfpr/espjava/backendcidades/visao/CidadeController.java
+++ 6-java-web/projetos/backend-cidades/src/main/java/br/edu/utfpr/espjava/backendcidades/visao/CidadeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/api/v1")
 public class CidadeController {
 
     @GetMapping("/")
